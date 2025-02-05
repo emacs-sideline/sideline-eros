5c476b24a3e8cfc61e71b9d7b4989ee2dbff2dc0
Jen-Chieh Shen <jcs090218@gmail.com>
2025-02-05 15:47:02 -0800

    docs: Add commentary

--- sideline-eros.el
+++ sideline-eros.el
@@ -28,6 +28,14 @@
 ;;
 ;; Show EROS result with sideline.
 ;;
+;; 1) Add sideline-eros to sideline backends list,
+;;
+;;   (setq sideline-backends-right '(sideline-eros))
+;;
+;; 2) Then enable sideline-mode in the target buffer,
+;;
+;;   M-x sideline-mode
+;;
 
 ;;; Code:
 
